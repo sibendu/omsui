12420683323f6d1e1d382b7dfef5fc9859e399b6
320086703 <320086703@YY193506.code1.emi.philips.com>
2020-07-13 23:35:06 +0530

    .

--- src/main/java/sd/oms/util/OMSUtil.java
+++ src/main/java/sd/oms/util/OMSUtil.java
@@ -1,28 +1,31 @@
 package sd.oms.util;
 
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.util.Properties;
+import java.io.*;
+import java.net.*;
+import java.util.*;
 
-import com.google.cloud.storage.BlobId; 
+import com.google.cloud.storage.BlobId;
 import com.google.cloud.storage.BlobInfo;
 import com.google.cloud.storage.Storage;
 import com.google.cloud.storage.StorageOptions;
-
+import com.google.common.collect.Lists;
 import com.google.auth.oauth2.*;
 import com.google.cloud.storage.*;
 
+
+import org.springframework.web.multipart.MultipartFile;
+
 public class OMSUtil {
-	
+
 	public static String IMAGE_BASE_URL = "https://storage.cloud.google.com/oms_pictures/";
 	public static String IMAGE_URL_SUFFIX = ".jpg?authuser=3";
+	public static String GCP_BUCKET = "oms_pictures";
+	public static String GCP_AUTH_FILE = "base-project-f69ff8d47af1.json";
 	
 	public static String USER_TYPE_SELLER = "SELLER";
 	public static String USER_TYPE_CUSTOMER = "CUSTOMER";
 	public static String USER_TYPE_ADMIN = "ADMIN";
-	
-	
+
 	public static String ORDER_STATUS_NEW = "New";
 	public static String ORDER_STATUS_PROCESSING = "Processing";
 	public static String ORDER_STATUS_CANCELLED = "Cancelled";
@@ -32,60 +35,49 @@ public class OMSUtil {
 	public static String ITEM_STATUS_ORDERED = "Ordered";
 	public static String ITEM_STATUS_CANCELLED = "Cancelled";
 	public static String ITEM_STATUS_NOT_AVAILABLE = "Not Available";
-	
+
 	public static void main(String[] args) throws Exception {
-
-		// Recipient's email ID needs to be mentioned.
-		String to = "fromaddress@gmail.com";
 		
-		// Instantiates a client
-	    Storage storage = StorageOptions.getDefaultInstance().getService();
-
-	    // The name for the new bucket
-	    String bucketName = args[0];  // "my-new-bucket";
-
-	    // Creates the new bucket
-	    Bucket bucket = storage.create(BucketInfo.of(bucketName));
-
-	    System.out.printf("Bucket %s created.%n", bucket.getName());
+		String path = "C:\\tmp\\140.jpg";
+		FileInputStream fis = new FileInputStream(new File(path));
+		byte[] b = new byte[10000];
+		fis.read(b);
+		fis.close();
 		
-		uploadObject(null, null, null, null);
+		uploadObject(new Long(1001), "150.jpg", null);
 	}
-
-	public static void uploadObject(String projectId, String bucketName, String objectName, String filePath)
+	
+	
+	public static void uploadObject(Long sellerId, String objectName, MultipartFile file)
 			throws IOException {
-
-		projectId = "ecomm-280407";
-		bucketName = "oms_pictures";
-		objectName = "test-no-image.jpg";
-		filePath = "C:\\Incub\\Workspace\\oms\\oms_pictures\\no-image.jpg";
 		
-		String jsonPath = "base-project-f69ff8d47af1.json";
+		objectName = sellerId + "/" + objectName;		
 		
 		try {
-//			System.out.println("Here I am");	
-//			GoogleCredentials credentials = GoogleCredentials.fromStream(new FileInputStream(jsonPath))
-//					.createScoped(Lists.newArrayList("https://www.googleapis.com/auth/cloud-platform"));
-//			System.out.println(credentials);
-//			Storage store = StorageOptions.newBuilder().setCredentials(credentials).build().getService();
-//			System.out.println(store);
-//			
+
+			InputStream in = OMSUtil.class.getClassLoader().getResourceAsStream(GCP_AUTH_FILE);
+			GoogleCredentials credentials = GoogleCredentials.fromStream(in)
+					.createScoped(Lists.newArrayList("https://www.googleapis.com/auth/cloud-platform"));
+			Storage storage = StorageOptions.newBuilder().setCredentials(credentials).build().getService();
+			
 //			System.out.println("Buckets:");
 //			Storage.Buckets.List buckets = store.buckets().list();
-//			for (Bucket bucket : buckets.iterateAll()) {
+//			for (Bucket bucket : buckets.iterateAll()) {d:
 //				System.out.println(bucket.toString());
 //			}
 
-			Storage storage = StorageOptions.newBuilder().setProjectId(projectId).build().getService();
-			BlobId blobId = BlobId.of(bucketName, objectName);
+			BlobId blobId = BlobId.of(GCP_BUCKET, objectName);
 			BlobInfo blobInfo = BlobInfo.newBuilder(blobId).build();
-			storage.create(blobInfo, Files.readAllBytes(Paths.get(filePath)));
+			//storage.create(blobInfo, Files.readAllBytes(Paths.get(filePath)));
+			storage.create(blobInfo,  file.getBytes());
+			
+			System.out.println("File upload :: " + objectName);
+			
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
-			e.printStackTrace();
+			//e.printStackTrace();
 			System.out.println("Error: " + e.getMessage());
 		}
-
-		System.out.println("File " + filePath + " uploaded to bucket " + bucketName + " as " + objectName);
+		
 	}
 }
